54f4e8a6f481898cfc06fac10317bbdb52ab9013
Elliot Mace <elliotmace2109@icloud.com>
2026-02-26 13:53:12 +0000

    Calc in Python Simple

---
+++ Calc.py
@@ -0,0 +1,37 @@
+##python script##
+
+##Simple Calculator##
+
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2 
+
+print("Please seleect the operation you wish to use:\n")
+print("1. Addition\n")
+print("2. Subtraction\n")
+print("3. Multiplication\n")
+print("4. Division\n") 
+
+sel = int(input("Select operation (1-4): "))
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+if sel == 1:
+    print(n1, "+", n2, "=", add(n1, n2))
+elif sel == 2:
+    print(n1, "-", n2, "=", sub(n1, n2))
+elif sel == 3:
+    print(n1, "*", n2, "=", mul(n1, n2))
+elif sel == 4:
+    print(n1, "/", n2, "=", div(n1, n2))
+else:
+    print("Invalid input")
